550586246290be4ac80c94b8b09933a4e8a1929b
Frank Pigeon Jr. <frank.pigeonjr@gmail.com>
2026-03-02 09:49:48 -0600

    feat: adds dynamic image for selected user

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { DUMMY_USERS } from "../dummy-users";
+
+const randomUserId = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
-  selector: 'app-user',
+  selector: "app-user",
   standalone: true,
   imports: [],
-  templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  templateUrl: "./user.component.html",
+  styleUrl: "./user.component.css",
 })
 export class UserComponent {
-
+  selectedUser = DUMMY_USERS[randomUserId];
 }
